e706e3f6c2f5fe9cd4dc6701744e703e301e358f
RobertMudzonga <robertmudzonga@yahoo.com>
2021-08-26 06:28:26 -0700

    Advance task 9

---
+++ 0x00-hello_world/101-quote.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+#include <unistd.h>
+
+/**
+ *main - prints exactly "and the piece of art is useful"
+ *- Dora Korpar 2015-10-19
+ *followed by a new line, to the standard error
+ *Return: Always 1 (Succes)
+ */
+
+int main(void)
+{
+write(2, "and the piece of art is useful\" -Dora Korpar 2015-10-19\n", 59);
+return (1);
+}
