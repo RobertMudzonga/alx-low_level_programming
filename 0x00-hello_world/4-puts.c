c94a4ed6157588f3078c41c7aae80f6106076da1
RobertMudzonga <robertmudzonga@yahoo.com>
2021-08-26 04:17:07 -0700

    Modified puts

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,10 +1,11 @@
 #include <stdio.h>
 
+/**
+ *main-Entry point
+ *Return:Always 0 (Success) 
+*/
 int main(void)
-
 {
- 
-    puts ("\"Programming is like building a multilingual puzzel");
+  puts ("\"Programming is like building a multilingual puzzel")
     return (0);
-
 }
