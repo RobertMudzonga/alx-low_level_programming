e18add865ebdcb0ca38e2ee556b7c20709f16b4a
RobertMudzonga <robertmudzonga@yahoo.com>
2021-08-26 06:06:17 -0700

    size

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -1,8 +1,8 @@
 #include <stdio.h>
 
 /**
- *main - prints the size of various types
- *Return - always 0 success if not zero otherwise
+ *main - Prints the size of various types
+ *Return: Always 0 success if not zero otherwise
  */
 
 int main(void)
