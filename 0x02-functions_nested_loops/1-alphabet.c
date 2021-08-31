ffbdb49830eded81d813ec5ab679507ef2ec6aaa
RobertMudzonga <robertmudzonga@yahoo.com>
2021-08-31 04:14:48 -0700

    alphabet

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,21 @@
+#include "main.h"
+
+/**
+ *main- prints lowercase, followed by new line
+ *Return: Always 0 sucess
+ */
+
+void print_alphabet(void)
+{
+char ch;
+
+ch = 'a';
+
+while(ch <= 'z')
+{
+_putchar(ch);
+ch++
+}
+_putchar('\n');
+return (0);
+}
