5a40b0890d1fcbd533bda333b9d50620bb385c24
RobertMudzonga <robertmudzonga@yahoo.com>
2021-08-31 03:56:06 -0700

    holberton

---
+++ 0x02-functions_nested_loops/0-holberton.c
@@ -0,0 +1,22 @@
+#include "main.h"
+
+/**
+ *main - print holberton, followed by new line
+ *Retun: Always 0 sucess
+ */
+
+int main(void)
+{
+_putchar('H');
+_putchar('o');
+_putchar('l');
+_putchar('b');
+_putchar('e');
+_putchar('r');
+_putchar('t');
+_putchar('o');
+_putchar('n');
+_putchar('\n');
+
+return (0);
+}
