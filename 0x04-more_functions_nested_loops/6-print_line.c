041efcf129ad456e37e01c67c081ac39bd0e70a0
RobertMudzonga <robertmudzonga@yahoo.com>
2021-09-02 07:58:17 -0700

    corrected 0, 6 & 7

--- 0x04-more_functions_nested_loops/6-print_line.c
+++ 0x04-more_functions_nested_loops/6-print_line.c
@@ -1,5 +1,5 @@
 #include "main.h"
-
+#include <stdio.h>
 /**
  * print_line - draws a straight line in the terminal
  * @n: number of times the character _ should be printed
--- 0x04-more_functions_nested_loops/7-print_diagonal.c
+++ 0x04-more_functions_nested_loops/7-print_diagonal.c
@@ -1,5 +1,5 @@
 #include "main.h"
-
+#include <stdio.h>
 /**
  * print_diagonal - draws a diagonal line on the terminal
  * @n: number of times the character \ should be printed
