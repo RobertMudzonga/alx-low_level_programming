0ac319c852b7b8d960e58c7f74cfb27b5f9194a2
RobertMudzonga <robertmudzonga@yahoo.com>
2021-09-06 01:24:47 -0700

    9 corrected

--- 0x05-pointers_arrays_strings/9-strcpy.c
+++ 0x05-pointers_arrays_strings/9-strcpy.c
@@ -38,15 +38,3 @@ dest[i] = src[i];
 return (dest);
 }
 
-/* another method using pointer notation
-char *_strcpy(char *dest, char *src)
-{
-while (*src)
-{
-*dest = *src;
-src++;
-dest++;
-}
-return (dest);
-}
-*/
