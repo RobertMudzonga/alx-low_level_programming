48b0fd925791a63cac70c157f2dd8c79790687e1
RobertMudzonga <robertmudzonga@yahoo.com>
2021-09-30 09:11:54 -0700

    1-print_numbers correct

--- 0x10-variadic_functions/1-print_numbers.c
+++ 0x10-variadic_functions/1-print_numbers.c
@@ -15,11 +15,11 @@ va_start(list, n);
 for (i = 0; i < n; i++)
 {
 if (!separator)
-printf("%d", va_arg(list. int));
+printf("%d", va_arg(list, int));
 else if (separator && i == 0)
 printf("%d", va_arg(list, int));
 else
-  printf("%s%d", separator, va_arg(list, int));
+printf("%s%d", separator, va_arg(list, int));
 }
 
 va_end(list);
