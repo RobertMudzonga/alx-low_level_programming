297fb47ec0428a06fdcc7f011062f4d5311725c8
RobertMudzonga <robertmudzonga@yahoo.com>
2021-10-14 11:09:59 -0700

    2 corrected

--- 0x14-bit_manipulation/2-get_bit.c
+++ 0x14-bit_manipulation/2-get_bit.c
@@ -7,14 +7,14 @@
  *
  * Return: value of the bit
  */
-int get_bit(unsigned long in n, unsigned int index)
+int get_bit(unsigned long int n, unsigned int index)
 {
-int bit_val;
-
-if (index > 63)
+unsigned long int divisor, check;
+if (index > (sizeof(unsigned long int) * 8 - 1))
 return (-1);
-
-bit_val = (n >> index) & 1;
-
-return (bit_val);
+divisor = 1 << index;
+check = n & divisor;
+if (check == divisor)
+return (1);
+return (0);
 }
