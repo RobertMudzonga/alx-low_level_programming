58e7327260b0d3a5c283abd6e9056a408f921d9d
Robert Mudzonga <88309711+RobertMudzonga@users.noreply.github.com>
2022-06-30 14:31:05 +0200

    Update 1-binary.c

--- 0x1E-search_algorithms/1-binary.c
+++ 0x1E-search_algorithms/1-binary.c
@@ -1,61 +1,61 @@
 #include "search_algos.h"
-
 /**
- * recursive_search - searches for a value in an array of
- * integers using the Binary search algorithm
- *
- *
- * @array: input array
- * @size: is the number of elements in array
- * @value: value to search for
- * Return: index of the number
- */
-int recursive_search(int *array, size_t size, int value)
+* prar - prints array
+* @array: array
+* @start: start point
+* @end: ending point
+* Return: nothing
+**/
+void prar(int *array, size_t start, size_t end)
 {
-  size_t half = size / 2;
-  size_t i;
 
-  if (array == NULL || size == 0)
-    return (-1);
+	unsigned int i;
 
-  printf("Searching in array");
-
-  for (i = 0; i < size; i++)
-    printf("%s %d", (i == 0) ? ":" : ",", array[i]);
-
-  printf("\n");
-
-  if (half && size % 2 == 0)
-    half--;
-
-  if (value == array[half])
-    return ((int)half);
-
-  if (value < array[half])
-    return (recursive_search(array, half, value));
-
-  half++;
-
-  return (recursive_search(array + half, size - half, value) + half);
+	printf("Searching in array: ");
+	for (i = start; i < end; i++)
+	{
+		printf("%d, ", array[i]);
+	}
+	printf("%d\n", array[end]);
 }
 
 /**
- * binary_search - calls to binary_search to return
- * the index of the number
- *
- * @array: input array
- * @size: size of the array
- * @value: value to search in
- * Return: index of the number
- */
+* binary_search - a function that searches for a value in a
+*sorted array of integers using the Binary search algorithm
+* @array: pointer to the first element of the array
+* @size: is the number of elements in array
+* @value: value to search for
+* Return: index OR -1
+**/
 int binary_search(int *array, size_t size, int value)
 {
-  int index;
+	unsigned int left = 0, right;
+	int middle;
 
-  index = recursive_search(array, size, value);
+	right = size - 1;
 
-  if (index >= 0 && array[index] != value)
-    return (-1);
-
-  return (index);
+	if (array == NULL)
+		return (-1);
+	if (size == 1)
+	{
+		return (array[left]);
+	}
+	while (right >= left)
+	{
+		if (left == 0 && right == 0)
+			return (-1);
+		prar(array, left, right);
+		middle = (right + left) / 2;
+		if (array[middle] == value)
+			return (array[middle]);
+		else if (array[middle] < value)
+		{
+			left = middle + 1;
+		}
+		else if (array[middle] > value)
+		{
+			right = middle;
+		}
+	}
+	return (-1);
 }
